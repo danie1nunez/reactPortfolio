fe3bc03e036402f898af5c7ba37465f5cd814b20
Daniel Nunez <161102831+danie1nunez@users.noreply.github.com>
2024-08-20 00:53:38 -0400

    forgot more

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
 import '../index.css';
-
+import github from "../assets/github.png"
+import linkedin from "../assets/linkedin.png"
+import stack from "../assets/stack.png"
 export default function Contact() {
     return (
         <div className='min-h-screen flex justify-center'>
@@ -8,13 +10,13 @@ export default function Contact() {
                 <p className='mb-4 text-lg'>email: danny03nunez@gmail.com</p>
                 <div className='flex flex-row'>
                     <a href="https://github.com/danie1nunez">
-                        <img className='w-16 h-16 mx-4' src="./src/assets/github.png" alt="github" />
+                        <img className='w-16 h-16 mx-4' src={github} alt="github" />
                     </a>
                     <a href="https://www.linkedin.com/in/daniel-nunez-b54a49321/">
-                        <img className='w-16 h-16 mx-4' src="./src/assets/linkedin.png" alt="linkedin" />
+                        <img className='w-16 h-16 mx-4' src={linkedin} alt="linkedin" />
                     </a>
                     <a href="https://stackoverflow.com/users/26664550/danthedev">
-                        <img className='w-16 h-16 mx-4' src="./src/assets/stack.png" alt="stack overflow" />
+                        <img className='w-16 h-16 mx-4' src={stack} alt="stack overflow" />
                     </a>
                 </div>
             </div>
